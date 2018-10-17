2bbef19092f178cfd38391d7a59fcab7d5e1564d
Alex Weinberger <alexweinberger7@gmail.com>
2018-10-17 10:15:54 -0400

    refactoring org structure

--- GeneralizingMajerciksCode/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args ) {
-      BasicPSO x = new BasicPSO();
-      x.setup();
-      for(int i = 0; i < 100; i++){
-        x.draw();
-      }
-    }
-}
